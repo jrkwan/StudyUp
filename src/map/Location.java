64752700de5af46940295f35ce6c45ec88cb1319
jrkwan <jrkwan@ucdavis.edu>
2019-01-17 17:53:16 -0800

    Introduces Location object

---
+++ src/map/Location.java
@@ -0,0 +1,29 @@
+package map;
+
+public class Location {
+
+	private double lat;
+	private double lon;
+
+	public Location(double lat, double lon) {
+		this.setLat(lat);
+		this.setLon(lon);
+	}
+
+	public double getLon() {
+		return lon;
+	}
+
+	public void setLon(double lon) {
+		this.lon = lon;
+	}
+
+	public double getLat() {
+		return lat;
+	}
+
+	public void setLat(double lat) {
+		this.lat = lat;
+	}
+
+}
